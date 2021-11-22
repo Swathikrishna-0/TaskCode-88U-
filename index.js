68eb880a8e9526a6cc594aa26aab363ce41e2f6a
Swathikrishna-0 <ssspriyamoru@gmail.com>
2021-11-22 14:38:10 +0530

    updted auto suggesting

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ new Awesomplete(input, {
 	list: [
 		{ label: "234234", value: "Andhra Pradesh" },
 		{ label: "234576", value: "Tamil Nadu" },
-		{ label: "987635", value: "Uttar pradesh" }
+		{ label: "987635", value: "Uttar pradesh" },
+    { label: "123456", value: "Odisha" },
+    { label: "983286", value: "Arunachal Pradesh" },
+    { label: "725346", value: "Karnataka" },
+    { label: "123476", value: "Madhya Pradesh" },
 	]
 });
 new Awesomplete(inputReference, {
