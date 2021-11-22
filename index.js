e446d2352d0ca00ff036d6be68173c9f6e5c80da
Swathikrishna-0 <ssspriyamoru@gmail.com>
2021-11-22 14:33:13 +0530

    added autosuggesting

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ btn.onclick = function(){
 span.onclick = function(){
   modal.style.display = "none";
 }
+
+
+var input = document.getElementById("pincode");
+
+new Awesomplete(input, {
+	list: [
+		{ label: "234234", value: "Andhra Pradesh" },
+		{ label: "234576", value: "Tamil Nadu" },
+		{ label: "987635", value: "Uttar pradesh" }
+	]
+});
+new Awesomplete(inputReference, {
+	minChars: 6,
+	maxItems: 5,
+});
